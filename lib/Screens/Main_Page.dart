2280ad8f25d0209b49df19b5e362a96446e8ab6a
arora <aroramadhav65@gmail.com>
2022-04-14 14:23:55 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  MainPage({ Key? key }) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
