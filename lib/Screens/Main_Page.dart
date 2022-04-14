2c2f38e15fc2fbf48fb534770be5ef724b21f5d6
arora <aroramadhav65@gmail.com>
2022-04-14 22:10:10 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -311,6 +311,7 @@ class _MainPageState extends State<MainPage> {
                   ),
                   Container(
                     height: 192,
+                    child: Placeholder(),
                   ),
                 ],
               ),
