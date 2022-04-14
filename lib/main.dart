7bafe1deaf03d19d294820fe1fb139d312cf083c
arora <aroramadhav65@gmail.com>
2022-04-14 18:56:39 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vatika',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
