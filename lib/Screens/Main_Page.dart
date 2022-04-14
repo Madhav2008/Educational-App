f618f206d94a11b24df7a402eeb133aca9805b1c
arora <aroramadhav65@gmail.com>
2022-04-14 20:12:40 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -223,7 +223,7 @@ class _MainPageState extends State<MainPage> {
                         SizedBox(
                           height: 8,
                         ),
-                        Text('Category'),
+                        Text('Leaderboard'),
                       ],
                     ),
                   ),
