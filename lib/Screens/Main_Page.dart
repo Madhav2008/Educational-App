0f432085014efaa2c7af50857fcefe372fea68cb
arora <aroramadhav65@gmail.com>
2022-04-14 23:51:01 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -670,6 +670,7 @@ class _MainPageState extends State<MainPage> {
                       ),
                     ],
                   ),
+                  Text('Join now')
                 ],
               ),
             ),
