824271c6d8cd15889b9696d9a10b1e6acbc258df
arora <aroramadhav65@gmail.com>
2022-04-14 22:09:32 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -306,6 +306,7 @@ class _MainPageState extends State<MainPage> {
                     ),
                   ),
                   Text('you may also like'),
+                  SizedBox(height: 16,)
                 ],
               ),
             ),
