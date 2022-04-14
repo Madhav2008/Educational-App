99564fed440ba46f30da4df3612b0c3c055a8229
arora <aroramadhav65@gmail.com>
2022-04-14 23:23:08 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -358,6 +358,10 @@ class _MainPageState extends State<MainPage> {
                                           Icons.star,
                                           color: Colors.yellow,
                                         ),
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                        ),
                                       ],
                                     ),
                                   ],
