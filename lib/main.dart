b98fb8ea37d6e88dfd450094eeffe329bd7ac20c
arora <aroramadhav65@gmail.com>
2022-04-14 14:23:23 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vatika',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: MainPage(),
     );
