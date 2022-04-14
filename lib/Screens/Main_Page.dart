40b022dfbd826654aff9ba417e0cc3306d44d23b
arora <aroramadhav65@gmail.com>
2022-04-14 19:06:40 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -49,6 +49,7 @@ class _MainPageState extends State<MainPage> {
                       ],
                     ),
                     Spacer(),
+                    // Badge
                   ],
                 ),
               ),
