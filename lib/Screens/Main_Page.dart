4dc95cf3e341876aab1a6d091d8a4cac4596aaac
arora <aroramadhav65@gmail.com>
2022-04-14 20:26:46 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -117,7 +117,10 @@ class _MainPageState extends State<MainPage> {
             GridView.count(
               crossAxisCount: 3,
               shrinkWrap: true,
-              padding: EdgeInsets.only(top: 8, bottom: 7,),
+              padding: EdgeInsets.only(
+                top: 8,
+                bottom: 7,
+              ),
               children: <Widget>[
                 Container(
                   child: Column(
