d5ecc7db5b640dded90c2311f6e2bdf795a8c439
arora <aroramadhav65@gmail.com>
2022-04-14 14:21:35 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1,3 +1,14 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
