9bb27424e48acac796b0e3356f55d88097cab929
arora <aroramadhav65@gmail.com>
2022-04-14 22:08:19 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -294,7 +294,7 @@ class _MainPageState extends State<MainPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.all(8.0),
+                    padding: EdgeInsets.only(right: 24.0),
                     child: Row(
                       children: <Widget>[
                         Text('Recommended course'),
