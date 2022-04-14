bbef8af0127c570241616ab6059e79f99765c325
arora <aroramadhav65@gmail.com>
2022-04-14 20:46:04 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -293,7 +293,9 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[],
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home),),
+        ],
       ),
     );
   }
