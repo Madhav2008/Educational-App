5d0e8ef9632614e4fd664c642b47460f0fccbd74
arora <aroramadhav65@gmail.com>
2022-04-14 22:20:36 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -328,6 +328,7 @@ class _MainPageState extends State<MainPage> {
                           width: 140,
                           margin: EdgeInsets.only(
                             right: 16,
+                            bottom: 8,
                           ),
                           decoration: BoxDecoration(
                             color: Colors.red,
