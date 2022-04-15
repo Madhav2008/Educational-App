27ca33578c884153adb5bea966c61103d0e7ada9
arora <aroramadhav65@gmail.com>
2022-04-15 17:11:32 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -293,6 +293,7 @@ class _MainPageState extends State<MainPage>
                           child: Icon(
                             Icons.video_camera_back,
                             color: Colors.white,
+                            size: 30
                           ),
                         ),
                       ),
