7828bb47c257fff415da289ab59213319dea1ca6
arora <aroramadhav65@gmail.com>
2022-04-15 15:46:42 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1235,7 +1235,7 @@ class _MainPageState extends State<MainPage>
                                       children: <Widget>[
                                         Text(
                                           'Record',
-                                          style: TextStyle(fontSize: 12),
+                                          style: TextStyle(fontSize: 12,),
                                         ),
                                         Spacer(),
                                         Icon(
