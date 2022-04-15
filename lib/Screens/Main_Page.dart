42653b447cbe3f308c7a76031ffd0e4433e6ca86
arora <aroramadhav65@gmail.com>
2022-04-15 13:30:56 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1149,7 +1149,7 @@ class _MainPageState extends State<MainPage>
                 indicatorSize: TabBarIndicatorSize.label,
                 indicatorColor: Colors.teal,
                 indicator: UnderlineTabIndicator(
-                  borderSide: BorderSide(),
+                  borderSide: BorderSide(width: 2.0),
                 ),
                 tabs: <Widget>[
                   Tab(
