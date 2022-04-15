279f11206d547ec720380cfe091f22bd3da39eea
arora <aroramadhav65@gmail.com>
2022-04-15 15:39:40 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1240,6 +1240,7 @@ class _MainPageState extends State<MainPage>
                                         Spacer(),
                                         Icon(Icons.message),
                                         Text('98'),
+                                        Spacer()
                                       ],
                                     ),
                                   ],
