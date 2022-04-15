6ef91e35055c89762099e39bfad984dfad247a7b
arora <aroramadhav65@gmail.com>
2022-04-15 12:09:54 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1088,7 +1088,7 @@ class _MainPageState extends State<MainPage> {
                         width: 12,
                         decoration: BoxDecoration(
                           color: Colors.grey,
-                          borderRadius: BorderRadius.circular(8),
+                          borderRadius: BorderRadius.circular(4),
                         ),
                       ),
                       Container(
