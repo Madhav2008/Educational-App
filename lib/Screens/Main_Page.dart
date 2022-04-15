fc5a46247bf4088a8b55734db443fcfac45934ea
arora <aroramadhav65@gmail.com>
2022-04-15 15:27:38 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1213,7 +1213,7 @@ class _MainPageState extends State<MainPage>
                               child: Container(
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(12),
-                                  color: Colors.lightGreenAccent
+                                  color: Colors.lightGreenAccent,
                                 ),
                               ),
                             ),
