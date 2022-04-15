b13c957069330fbc67501729fea0d9743f9c9a4c
arora <aroramadhav65@gmail.com>
2022-04-15 11:27:11 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -307,7 +307,7 @@ class _MainPageState extends State<MainPage> {
                         Spacer(),
                         Text(
                           'More',
-                          style: TextStyle(fontSize: 12),
+                          style: TextStyle(fontSize: 12,),
                         ),
                       ],
                     ),
