6a26aebe2f435b874b5aed82296e6737431a870d
arora <aroramadhav65@gmail.com>
2022-04-15 16:30:32 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1304,7 +1304,7 @@ class _MainPageState extends State<MainPage>
                       return Divider();
                     },
                   ),
-                  !dummyLoading ? CircularProgressIndicator():
+                  !dummyLoading ? CircularProgressIndicator(): Placeholder(),
                   Placeholder(),
                   Placeholder(),
                   Placeholder(),
