cc1b4be82f84a2552c47a27f692e238cf1ab9d2f
arora <aroramadhav65@gmail.com>
2022-04-15 16:22:15 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1249,7 +1249,9 @@ class _MainPageState extends State<MainPage>
                                           size: 12,
                                           color: Colors.grey,
                                         ),
-                                        SizedBox(width: 8,),
+                                        SizedBox(
+                                          width: 8,
+                                        ),
                                         Text(
                                           '122',
                                           style: TextStyle(
