d273173abf28155b6e4cec24bf77c9f0096a18d6
arora <aroramadhav65@gmail.com>
2022-04-15 13:07:36 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1137,6 +1137,7 @@ class _MainPageState extends State<MainPage>
             ),
             Container(
               height: 32,
+              child: TabBar(),
             ),
           ],
         ),
