2b65223cbb0d30775ea331cb178cb88c1290d684
arora <aroramadhav65@gmail.com>
2022-04-15 13:37:44 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1186,6 +1186,7 @@ class _MainPageState extends State<MainPage>
                 right: 24,
                 top: 8,
               ),
+              height: 380,
             ),
           ],
         ),
