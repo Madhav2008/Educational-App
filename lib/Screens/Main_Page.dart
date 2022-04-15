20385455e8e2244708fb9ca0c54518e8541b7a75
arora <aroramadhav65@gmail.com>
2022-04-15 17:00:59 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -235,7 +235,7 @@ class _MainPageState extends State<MainPage>
                           ],
                         ),
                         child: Center(
-                          child: Icon(),
+                          child: Icon(Icons.store),
                         ),
                       ),
                       SizedBox(
