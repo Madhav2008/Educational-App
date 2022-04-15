d5ceadb14a8aaabefdbc5c10d5ff2c48c8d5c5e7
arora <aroramadhav65@gmail.com>
2022-04-15 13:50:57 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1061,9 +1061,9 @@ class _MainPageState extends State<MainPage>
                   ),
                   Container(
                     height: 150,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 16,
-                    ),
+                    // margin: EdgeInsets.symmetric(
+                    //   vertical: 16,
+                    // ),
                     decoration: BoxDecoration(
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(
