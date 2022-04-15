6bc44e3adb7a7b6db838dfb679945d82c2a4fd0f
arora <aroramadhav65@gmail.com>
2022-04-15 16:43:59 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1350,7 +1350,7 @@ class _MainPageState extends State<MainPage>
                                             CrossAxisAlignment.start,
                                         children: <Widget>[
                                           Text(
-                                            'What is used in',
+                                            'What is used in life',
                                             style: TextStyle(
                                               fontSize: 13,
                                             ),
