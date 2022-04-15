4acba4bd2eeec3cc1d13e8f23a93351b13d0c7df
arora <aroramadhav65@gmail.com>
2022-04-15 11:29:12 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -322,6 +322,7 @@ class _MainPageState extends State<MainPage> {
                     'you may also like',
                     style: TextStyle(
                       fontSize: 12,
+                      color: Colors.grey
                     ),
                   ),
                   SizedBox(
