2f4d8d5ebda74733c7f24a73a0d313761e42b0ec
arora <aroramadhav65@gmail.com>
2022-04-15 15:54:46 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1247,7 +1247,7 @@ class _MainPageState extends State<MainPage>
                                         Icon(
                                           Icons.favorite,
                                           size: 12,
-                                          color: Colors.grey
+                                          color: Colors.grey,
                                         ),
                                         Text(
                                           '122',
