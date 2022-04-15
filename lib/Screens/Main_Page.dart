33de9494cdb56b58acd1cd15820c2664e3101785
arora <aroramadhav65@gmail.com>
2022-04-15 17:10:57 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -191,6 +191,7 @@ class _MainPageState extends State<MainPage>
                           child: Icon(
                             Icons.play_circle_fill,
                             color: Colors.white,
+                            size: 30
                           ),
                         ),
                       ),
