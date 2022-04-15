7a7e4becb03536ec33462ce5e2aa41ed9fac48f9
arora <aroramadhav65@gmail.com>
2022-04-15 13:11:10 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1145,7 +1145,7 @@ class _MainPageState extends State<MainPage>
                   Tab(
                     text: 'Lecture',
                   ),
-                  Tab()
+                  Tab(),
                 ],
               ),
             ),
