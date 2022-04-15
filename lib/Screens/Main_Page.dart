3b8663493d9a142c64be73f0ad9e14ea457325cd
arora <aroramadhav65@gmail.com>
2022-04-15 12:07:22 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1091,7 +1091,7 @@ class _MainPageState extends State<MainPage> {
                       Container(
                         height: 4,
                         width: 20,
-                        decoration: BoxDecoration(),
+                        decoration: BoxDecoration(color: Colors.grey),
                       ),
                     ],
                   ),
