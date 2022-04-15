f8880e766d31e3e79d7089d4417979409eb39abe
arora <aroramadhav65@gmail.com>
2022-04-15 12:12:06 +0530

    Update

--- lib/Screens/Main_Page.dart
+++ lib/Screens/Main_Page.dart
@@ -1076,7 +1076,9 @@ class _MainPageState extends State<MainPage> {
                           borderRadius: BorderRadius.circular(4),
                         ),
                       ),
-                      SizedBox(width: 8,),
+                      SizedBox(
+                        width: 8,
+                      ),
                       Container(
                         height: 4,
                         width: 20,
